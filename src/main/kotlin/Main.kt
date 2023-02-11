7befadf02152addeebc81ce2adf7ff43d98d82f8
wainainalinahlinah <wainainalinahw@gmail.com>
2023-02-11 10:24:41 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    var fullName: String = "Maryanne Wanjiru"
+    println(fullName)
+
+    var age: Byte =24
+    println(age)
+
+    var phoneNumber: String = "0746852623"
+    println(phoneNumber)
+
+    var weight: Byte = 126
+    println(weight)
+
+    var citizen: Boolean =true or false
+    println (citizen)
+
+}
